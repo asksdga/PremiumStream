826aeea2a8a578a3fc1a5343414e7bd65c5c1832
asksdga <akusayangkamuselaludahgituaja@gmail.com>
2026-06-27 18:39:41 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+alert("LOGIN JS LOADED");
+
 import {
 
     auth,
@@ -8,66 +10,16 @@ import {
 
 from "./firebase.js";
 
+alert("FIREBASE LOADED");
+
 const googleBtn =
 
 document.getElementById(
     "googleBtn"
 );
 
-googleBtn.onclick =
-async ()=>{
+googleBtn.onclick = ()=>{
 
-    try{
-
-        const result =
-
-        await signInWithPopup(
-
-            auth,
-            provider
-
-        );
-
-        const user =
-        result.user;
-
-        localStorage.setItem(
-
-            "ps_user",
-
-            JSON.stringify({
-
-                uid:
-                user.uid,
-
-                name:
-                user.displayName,
-
-                email:
-                user.email,
-
-                picture:
-                user.photoURL
-
-            })
-
-        );
-
-        location.href =
-        "dashboard.html";
-
-    }
-
-    catch(error){
-
-        alert(
-
-            error.message
-
-        );
-
-        console.log(error);
-
-    }
+    alert("BUTTON CLICK");
 
 };
