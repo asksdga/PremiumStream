3b07025dac45583ada6d57a21b783a6a42b05fbc
asksdga <akusayangkamuselaludahgituaja@gmail.com>
2026-06-27 16:35:24 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,71 @@
+import {
 
+    auth,
+    provider,
+    signInWithPopup
+
+}
+
+from "./firebase.js";
+
+const btn =
+
+document.getElementById(
+    "googleBtn"
+);
+
+btn.onclick =
+async ()=>{
+
+    try{
+
+        const result =
+
+        await signInWithPopup(
+
+            auth,
+            provider
+
+        );
+
+        const user =
+        result.user;
+
+        localStorage.setItem(
+
+            "ps_user",
+
+            JSON.stringify({
+
+                uid:
+                user.uid,
+
+                name:
+                user.displayName,
+
+                email:
+                user.email,
+
+                picture:
+                user.photoURL
+
+            })
+
+        );
+
+        location.href =
+        "dashboard.html";
+
+    }
+
+    catch(error){
+
+        alert(
+
+            error.message
+
+        );
+
+    }
+
+};
